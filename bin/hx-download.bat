e90d4bca0917e11ac31947dfb2d5d014495d4998
k <k@klabz.org>
2024-04-15 12:11:24 +0200

    Revert "[CI] try without bat files"
    This reverts commit 12bd16072e3c8ab8e941732df92258b95d1944e3.

---
+++ bin/hx-download.bat
@@ -0,0 +1,8 @@
+@ECHO OFF
+
+SET ROOT=%~dp0
+SET ROOT=%ROOT%..
+SET VERSION=windows64_569e52e
+SET HAXE_STD_PATH=%ROOT%\build\%VERSION%\std
+
+CALL %ROOT%\build\%VERSION%\haxe.exe --cwd %ROOT% run-download.hxml %*
